ffb21c61fef8e3d12c32b627c5b82cf6ddae2111
3t13nn3 <etiennepenault1997@gmail.com>
2020-02-11 19:38:11 +0100

    Final It1 with doc

--- Game.java
+++ Game.java
@@ -235,11 +235,13 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+    	
+        System.out.println("You are lost fella. Good luck fella. You'll do it fella.");
         System.out.println();
+        /*System.out.println("Your command words are:");
+        System.out.println("   go quit help");*/
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+		parser.showCommands();
     }
 
     /** 
--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-
-
 public class Main {
 
+	/**
+	 * Main function...
+	 */
 	public static void main(String[] args){
 		Game myGame = new Game();
 		myGame.play();
